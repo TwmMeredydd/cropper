0bcf8f29c5fe28488e0c511d9b9b07804f0f2422
TwmMeredydd <49553663+TwmMeredydd@users.noreply.github.com>
2023-06-25 15:42:11 +0100

    Remove unnecessary import

--- src/main/java/io/github/eman7blue/cropper/item/CropperMinecartItem.java
+++ src/main/java/io/github/eman7blue/cropper/item/CropperMinecartItem.java
@@ -16,7 +16,6 @@ import net.minecraft.util.math.BlockPointer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldEvents;
 import net.minecraft.world.event.GameEvent;
 
 public class CropperMinecartItem extends Item {
